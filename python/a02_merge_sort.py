c19d0836a49bef0bc5e42bf004c72ed57bbdca0e
Sunaina Pai <sunainapai.in@gmail.com>
2018-05-10 20:37:48 +0530

    Merge sort in Python

---
+++ python/a02_merge_sort.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+def merge(A, p, q, r):
+    L = A[p:q+1]   # L = A[p..q]
+    R = A[q+1:r+1] # R = A[q+1..r]
+    n1 = len(L)    # n1 = q - p + 1
+    n2 = len(R)    # n2 = r - q
+
+    i = 0
+    j = 0
+    k = p
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i = i + 1
+        else:
+            A[k] = R[j]
+            j = j + 1
+        k = k + 1
+
+    while i < n1:
+        A[k] = L[i]
+        i = i + 1
+        k = k + 1
+
+    while j < n2:
+        A[k] = R[j]
+        j = j + 1
+        k = k + 1
+
+
+def merge_sort(A, p, r):
+    if p < r:
+        q = (p + r) // 2
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+
+
+if __name__ == '__main__':
+    A = [5, 4, 1, 3, 2, 6]
+    merge_sort(A, 0, len(A) - 1)
+    print(A)
---
+++ python/test/test_a02_merge_sort.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+
+import unittest
+from a02_merge_sort import merge, merge_sort
+
+
+class MergeSortTest(unittest.TestCase):
+    def test_merge_typical(self):
+        A = [1, 7, 9, 2, 6, 8]
+        merge(A, 0, 2, len(A) - 1)
+        self.assertEqual(A, [1, 2, 6, 7, 8, 9])
+
+    def test_merge_two_items(self):
+        A = [1, 7, 9, 2, 6, 8]
+        merge(A, 2, 2, 3)
+        self.assertEqual(A, [1, 7, 2, 9, 6, 8])
+
+    def test_merge_one_item(self):
+        A = [1, 7, 9, 2, 6, 8]
+        merge(A, 2, 2, 2)
+        self.assertEqual(A, [1, 7, 9, 2, 6, 8])
+
+    def test_merge_sort_typical(self):
+        A = [1, 7, 9, 2, 6, 8]
+        merge_sort(A, 0, len(A) - 1)
+        self.assertEqual(A, [1, 2, 6, 7, 8, 9])
+
+    def test_merge_sort_typical(self):
+        A = [1, 7, 9, 2, 6, 8]
+        merge_sort(A, 2, 3)
+        self.assertEqual(A, [1, 7, 2, 9, 6, 8])
+
+    def test_merge_one_item(self):
+        A = [1, 7, 9, 2, 6, 8]
+        merge_sort(A, 2, 2)
+        self.assertEqual(A, [1, 7, 9, 2, 6, 8])
