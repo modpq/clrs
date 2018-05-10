ca88127f3e61f503351d74472d6b9660407dffec
Sunaina Pai <sunainapai.in@gmail.com>
2018-05-10 20:52:46 +0530

    Fix test method names in Python merge sort tests

--- python/test/test_a02_merge_sort.py
+++ python/test/test_a02_merge_sort.py
@@ -26,12 +26,12 @@ class MergeSortTest(unittest.TestCase):
         merge_sort(A, 0, len(A) - 1)
         self.assertEqual(A, [1, 2, 6, 7, 8, 9])
 
-    def test_merge_sort_typical(self):
+    def test_merge_sort_two_items(self):
         A = [1, 7, 9, 2, 6, 8]
         merge_sort(A, 2, 3)
         self.assertEqual(A, [1, 7, 2, 9, 6, 8])
 
-    def test_merge_one_item(self):
+    def test_merge_sort_one_item(self):
         A = [1, 7, 9, 2, 6, 8]
         merge_sort(A, 2, 2)
         self.assertEqual(A, [1, 7, 9, 2, 6, 8])
