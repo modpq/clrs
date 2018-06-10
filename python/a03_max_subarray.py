c1e248eed04a16b70b759dffd5b44a0c08066fea
Sunaina Pai <sunainapai.in@gmail.com>
2018-06-10 14:56:16 +0530

    Max subarray in Python

---
+++ python/a03_max_subarray.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# Author: Sunaina Pai
+# License: MIT
+
+
+def find_max_crossing_subarray(A, low, mid, high):
+    assert low <= mid < high
+
+    sum_ = 0
+
+    for i in range(mid, low - 1, -1):
+        sum_ += A[i]
+        if i == mid or sum_ > left_sum:
+            left_sum = sum_
+            max_left = i
+
+    sum_ = 0
+
+    for j in range(mid + 1, high + 1):
+        sum_ += A[j]
+        if j == mid + 1 or sum_ > right_sum:
+            right_sum = sum_
+            max_right = j
+
+    return max_left, max_right, left_sum + right_sum
+
+
+def find_maximum_subarray(A, low, high):
+    if low == high:
+        return low, high, A[low]
+
+    mid = (low + high) // 2
+
+    left_low, left_high, left_sum = (
+            find_maximum_subarray(A, low, mid))
+
+    right_low, right_high, right_sum = (
+            find_maximum_subarray(A, mid + 1, high))
+
+    cross_low, cross_high, cross_sum = (
+            find_max_crossing_subarray(A, low, mid, high))
+
+    if left_sum >= right_sum and left_sum >= cross_sum:
+        return left_low, left_high, left_sum
+
+    if right_sum >= left_sum and right_sum >= cross_sum:
+        return right_low, right_high, right_sum
+
+    return cross_low, cross_high, cross_sum
+
+
+if __name__ == '__main__':
+    A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+    low, high, max_sum = find_maximum_subarray(A, 0, len(A) - 1)
+    print(low, high, max_sum)
---
+++ python/test/test_a03_max_subarray.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Author: Sunaina Pai
+# License: MIT
+
+
+import unittest
+from a03_max_subarray import find_max_crossing_subarray, find_maximum_subarray
+
+
+class MaxSubarrayTest(unittest.TestCase):
+    def test_find_max_crossing_subarray_typical(self):
+        A = [1, 7, 9, 2, 6, 8]
+        result = find_max_crossing_subarray(A, 0, 2, len(A) - 1)
+        self.assertEqual(result, (0, 5, 33))
+
+    def test_find_max_crossing_subarray_negatives(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_max_crossing_subarray(A, 0, 2, len(A) - 1)
+        self.assertEqual(result, (2, 5, 13))
+
+    def test_find_max_crossing_subarray_three_items(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_max_crossing_subarray(A, 2, 3, len(A) - 2)
+        self.assertEqual(result, (2, 4, 5))
+
+    def test_find_max_crossing_subarray_two_items(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_max_crossing_subarray(A, 2, 2, len(A) - 3)
+        self.assertEqual(result, (2, 3, 11))
+
+    def test_find_max_crossing_subarray_one_item(self):
+        A = [1, -7, 9, 2, -6, 8]
+        with self.assertRaises(AssertionError):
+            find_max_crossing_subarray(A, 2, 2, len(A) - 4)
+
+    def test_find_max_crossing_subarray_one_item_fail(self):
+        A = [1]
+        with self.assertRaises(AssertionError):
+            find_max_crossing_subarray(A, 0, 0, 0)
+
+    def test_find_maximum_subarray_typical(self):
+        A = [1, 7, 9, 2, 6, 8]
+        result = find_maximum_subarray(A, 0, len(A) - 1)
+        self.assertEqual(result, (0, 5, 33))
+
+    def test_find_maximum_subarray_negatives(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_maximum_subarray(A, 0, len(A) - 1)
+        self.assertEqual(result, (2, 5, 13))
+
+    def test_find_maximum_subarray_three_items(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_maximum_subarray(A, 2, len(A) - 2)
+        self.assertEqual(result, (2, 3, 11))
+
+    def test_find_maximum_subarray_two_items(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_maximum_subarray(A, 3, len(A) - 2)
+        self.assertEqual(result, (3, 3, 2))
+
+    def test_find_maximum_subarray_one_item(self):
+        A = [1, -7, 9, 2, -6, 8]
+        result = find_maximum_subarray(A, 2, len(A) - 4)
+        self.assertEqual(result, (2, 2, 9))
